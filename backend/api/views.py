a800ac916c0b858b0c8472de5f3e40eeef372b92
dihsarjr <mohammed1rashid786@gmail.com>
2024-07-22 20:59:45 +0530

    all

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,17 @@
 from django.http import JsonResponse
+import json
+import  pro
+
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message":"hi' there"})
+    body = request.body
+    print(body)
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['headers'] = dict(request.headers)
+    print(request.headers)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,5 +2,5 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint,json={"Nothing":"full english"})
 print(get_response.text)
